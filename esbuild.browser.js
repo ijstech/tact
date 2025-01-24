4517a402f98cbad66c0e5e49695c17555d169dee
Celina-IJS <celina.dev@instajs.com>
2025-01-24 23:43:13 +0700

    chore: remove bignumber.js on build

--- esbuild.browser.js
+++ esbuild.browser.js
@@ -9,6 +9,24 @@ async function readFile(fileName) {
     return result;
 }
 
+// const nodePolyfills = {
+//     "node:path": "path-browserify",
+//     "path": "path-browserify",
+//     "fs": "browserify-fs",
+//     "stream": "stream-browserify",
+//     "crypto": "crypto-browserify"
+// }
+// {
+//     name: 'node-polyfills',
+//     setup(build) {
+//       for (const [moduleName, modulePath] of Object.entries(nodePolyfills)) {
+//         build.onResolve({ filter: new RegExp(`^${moduleName}$`) }, () => ({
+//           path: require.resolve(modulePath),
+//         }));
+//       }
+//     },
+// }
+
 esbuild
     .build({
         entryPoints: ["dist/index.js"],
@@ -18,9 +36,8 @@ esbuild
         sourcemap: false,
         target: "esnext",
         format: "iife",
-        globalName: "TactCompiler",
         platform: "browser",
-        external: ["@scom/ton-core", "@ton/core"],
+        external: ["@scom/ton-core", "@ton/core", "bignumber.js"],
         plugins: [
             nodeModulesPolyfillPlugin({
                 globals: {
@@ -30,23 +47,20 @@ esbuild
                     fs: true,
                 },
             }),
-            // {
-            //     name: 'node-polyfills',
-            //     setup(build) {
-            //       for (const [moduleName, modulePath] of Object.entries(nodePolyfills)) {
-            //         build.onResolve({ filter: new RegExp(`^${moduleName}$`) }, () => ({
-            //           path: require.resolve(modulePath),
-            //         }));
-            //       }
-            //     },
-            // },
         ],
     })
     .then(async () => {
         console.log("Bundling browser completed!");
         let indexContent = await readFile("bundle/browser.js");
         indexContent = indexContent.replace(/@ton\/core/g, "@scom/ton-core");
-        await Fs.writeFile("bundle/browser.js", indexContent, "utf8");
+        await Fs.writeFile(
+            "bundle/browser.js",
+            `
+ "use strict";
+${indexContent}
+            `,
+            "utf8",
+        );
     })
     .catch((error) => {
         console.error("Bundling browser failed:", error);
